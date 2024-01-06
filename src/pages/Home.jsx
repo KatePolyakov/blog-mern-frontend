e6705be17a269c5dfb84c5ba86328dce037fc387
Kate <polyakovkate@gmail.com>
2024-01-05 20:32:34 -0800

    added redux for tags, post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import axios from '../axios';
+
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
@@ -8,10 +9,18 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
+import { fetchPosts, fetchTags } from '../redux/slices/posts'
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const { posts, tags, comments } = useSelector(state => state.posts);
+
+  const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
+
   useEffect(() => {
-    axios.get('/posts');
+    dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, []);
   return (
     <>
@@ -21,26 +30,22 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? (<Post key={index} isLoading={true}/>) : (
             <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
+              id={obj._id}
+              title={obj.title}
               imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-                fullName: 'Keff',
-              }}
-              createdAt={'January 04, 2024'}
-              viewsCount={150}
+              user={obj.user}
+              createdAt={obj.createdAt}
+              viewsCount={obj.viewsCount} 
               commentsCount={3}
-              tags={['react', 'fun', 'typescript']}
+              tags={obj.tags}
               isEditable
             />
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
@@ -58,7 +63,7 @@ export const Home = () => {
                 text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
               },
             ]}
-            isLoading={false}
+            isLoading={true}
           />
         </Grid>
       </Grid>
