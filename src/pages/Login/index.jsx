99fcd115ca4c9311b916b31ed708f6b78acc9fc4
Kate <polyakovkate@gmail.com>
2024-01-08 13:18:26 -0800

    created auth with react-hook-form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,57 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-import styles from "./Login.module.scss";
+import { useForm } from 'react-hook-form';
+
+import styles from './Login.module.scss';
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'all',
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
-        Вход в аккаунт
+        Login
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          {...register('email', { required: 'Enter your email' })}
+          className={styles.field}
+          label="E-Mail"
+          type='email'
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          fullWidth
+        />
+        <TextField
+          {...register('password', { required: 'Enter your password' })}
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          className={styles.field}
+          label="Password"
+          fullWidth
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Login
+        </Button>
+      </form>
     </Paper>
   );
 };
