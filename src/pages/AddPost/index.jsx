bdfcee45cc4b508b3b61fd963b73290627bbe3a5
Kate <polyakovkate@gmail.com>
2024-01-16 17:29:52 -0800

    send post on the backend

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useMemo, useRef, useState } from 'react';
+import { useCallback } from 'react';
+import { useNavigate, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import TextField from '@mui/material/TextField';
@@ -10,26 +11,68 @@ import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
+  const navigate = useNavigate();
+
   const isAuth = useSelector(selectIsAuth);
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
 
-  const [title, setTitle] = useState('');
-  const [tags, setTags] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleChangeFile = () => {};
+  const inputFileRef = useRef(null); //photo
+  const [imageUrl, setImageUrl] = useState('');
 
-  const onClickRemoveImage = () => {};
+  const [text, setText] = useState(''); //post hooks
+  const [title, setTitle] = useState(''); //title hooks
+  const [tags, setTags] = useState(''); //tags hooks
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (error) {
+      console.warn(error);
+      alert('Error');
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
+
+  const onChange = useCallback((value) => {
+    setText(value);
   }, []);
 
-  const options = React.useMemo(
+  const onSubmit = async () => {
+    try {
+      setIsLoading(true);
+
+      const fields = {
+        title,
+        imageUrl,
+        tags: tags.split(',', ''),
+        text,
+      };
+
+      const { data } = await axios.post('/posts', fields);
+
+      const id = data._id;
+
+      navigate(`/posts/${id}`);
+    } catch (error) {
+      console.warn(error);
+      alert('Error post');
+    }
+  };
+
+  const options = useMemo(
     () => ({
       spellChecker: false,
       maxHeight: '400px',
@@ -48,21 +91,19 @@ export const AddPost = () => {
     return <Navigate to="/" />;
   }
 
-  console.log(title, tags, value);
-
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
         Preview
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
-          Delete
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Delete
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
       )}
       <br />
       <br />
@@ -82,9 +123,9 @@ export const AddPost = () => {
         onChange={(e) => setTags(e.target.value)}
         fullWidth
       />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
+        <Button size="large" variant="contained" onClick={onSubmit}>
           Public
         </Button>
         <a href="/">
